971d0acd45a2039849e798017fee562af693c7c6
Nishant Kishore <ynm2@cdc.gov>
2024-02-06 11:36:47 -0500

    emptying out functions

--- sections/1/R/interpolate_pop.R
+++ sections/1/R/interpolate_pop.R
@@ -10,34 +10,5 @@
 #' @param end.year  an integer for the last year of data to end interpolation
 #' @returns A tibble with variables `state`, `county`, `year`, `pop`
 interpolate_pop <- function(pop_data, start.year, end.year){
-  us_pop |> 
-    #convert wide to long
-    tidyr::pivot_longer(dplyr::starts_with("pop"), names_to = "year", names_prefix = "pop") |> 
-    #this only contains a few years of data so we can linearly extrapolate
-    #to fill in the rest 
-    dplyr::mutate(
-      year = stringr::str_replace(year, "_", ""), 
-      year = as.numeric(year)
-    ) %>%
-    #everything inide the {} below references the output above as '.'
-    {
-      #bind full years of data with fitted full years
-      dplyr::bind_cols(
-        #create the "fake" dataset
-        tidyr::expand_grid(
-          state = "Georgia", 
-          county = unique(.$county),
-          year = start.year:end.year), 
-        "fit_pop" = predict.lm(
-          #linear model specific for  year and county
-          object = lm(value ~ year + county, data = .), 
-          #producing the same newdata as above
-          newdata = tidyr::expand_grid(
-            state = "Georgia", 
-            county = unique(.$county),
-            year = start.year:end.year)
-        )
-      )
-    } |> 
-    dplyr::mutate(pop = round(fit_pop, 0))
+
 }
