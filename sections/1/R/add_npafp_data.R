32502dd9329c73ef27b19e9d5c6791e12bb9bf81
Nishant Kishore <ynm2@cdc.gov>
2024-02-20 13:09:16 -0500

    closes #3 :sparkle: a function to do whatever

--- sections/1/R/add_npafp_data.R
+++ sections/1/R/add_npafp_data.R
@@ -13,6 +13,40 @@
 #' @returns a ggplot object 
 add_npafp_data <- function(input_plot, spatial_data, afp_data, pop_data, start.year, end.year){
 
+  #subset afp data so that is it specific for the years that I'm interested in 
+  int_data <- afp_data |> 
+    mutate(year = lubridate::year(date)) |> 
+    filter(year >= start.year & year <= end.year)
+  
+  #subset afp data so that it's only NPAFP
+  int_data <- int_data |> 
+    filter(classification == "NPAFP")
+  
+  #count total incidence of NPAFP per Year
+  int_data <- int_data |> 
+    group_by(state, county, year) |> 
+    summarise(count = n())
+  
+  #subset pop data so that it's only for our years of interest
+  int_pop <- pop_data |> 
+    filter(year >= start.year & year <= end.year)
+  
+  #merge pop and afp data together
+  int_data <- left_join(int_pop, int_data, by = c("state", "county", "year")) |> 
+    replace_na(list("count" = 0))
+  
+  #calculate the NPAFP rate 
+  int_data <- int_data |> 
+    mutate(npafp_rate = count/pop*100000)
+  
+  #merge NPAFP rate into spatial file 
+  int_spatial_data <- left_join(spatial_data, int_data, by = c("NAMELSAD" = "county"))
+  
+  #create my map
+  plot <- input_plot +
+    geom_sf(data = int_spatial_data, aes(fill = npafp_rate)) +
+    facet_wrap(~year)
+  
   return(plot)
   
 }
